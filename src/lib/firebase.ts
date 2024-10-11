91b015d0139349ff6a5a04d58c87050a106dd33c
abdul-jabbar-dev <abdul.jabbar.dev@gmail.com>
2024-10-11 22:06:01 +0600

    initialize

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9D0tRTNjhSpqS4H6O6TzdMNvATh21CVA",
+  authDomain: "invoncify-600c8.firebaseapp.com",
+  projectId: "invoncify-600c8",
+  storageBucket: "invoncify-600c8.appspot.com",
+  messagingSenderId: "73589093683",
+  appId: "1:73589093683:web:bb0b99472a06209d37fa46"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
